ea7c3efbf73121c2669329cd43807f4e38736402
“Segun <Oluwatayoo08@gmail.com>
2024-04-03 13:39:00 +0100

    Add server.js file with basic Express setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+  const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

cf64aaf20fea5d3e73f065b7b922bb08a134d62c
“Segun <Oluwatayoo08@gmail.com>
2024-04-03 14:40:25 +0100

    Added express-ejs-layouts and main route

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 
 const express = require('express');
+const expresslayouts = require('express-ejs-layouts');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expresslayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
